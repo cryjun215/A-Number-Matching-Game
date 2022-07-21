7699448fd907d3e9d59b10844fa06a6238bf874a
cryju <cryju@DESKTOP-9EBU2QH>
2022-07-21 23:25:51 +0900

    초기 커밋.

---
+++ A Number Matching Game/A Number Matching Game.c
@@ -0,0 +1,31 @@
+﻿#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+
+int main()
+{
+	int value, guess, count = 0;
+
+	srand((int)time(NULL));
+	value = rand() % 100;
+	while (1) {
+		printf("Guess the answer.: ");
+		scanf("%d", &guess);
+		if (value < guess) {
+			printf("The suggested integer is high.\n");
+			count++;
+		}
+		else if (value > guess) {
+			printf("The suggested integer is low\n");
+			count++;
+		}
+		else if (value = guess) {
+			count++;
+			printf("Congratulations! Counts:%d\n", count);
+			break;
+		}
+	}
+
+	return 0;
+}
