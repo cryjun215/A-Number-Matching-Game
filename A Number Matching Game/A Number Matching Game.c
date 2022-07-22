6e4219d67c84e8d90b1df35385f23f704dea668f
LeopoldBloom2K <cryjun21579@gmail.com>
2022-07-23 03:04:58 +0900

    Update A Number Matching Game.c
    Add do-while

--- A Number Matching Game/A Number Matching Game.c
+++ A Number Matching Game/A Number Matching Game.c
@@ -1,31 +1,26 @@
 ﻿#define _CRT_SECURE_NO_WARNINGS
 #include <stdio.h>
 #include <stdlib.h>
-#include <math.h>
+#include <time.h>
 
 int main()
 {
-	int value, guess, count = 0;
+	int value, guess = 0, count = 0;
 
 	srand((int)time(NULL));
 	value = rand() % 100;
-	while (1) {
-		printf("Guess the answer.: ");
+	do {
+		printf("Guess the answer: ");
 		scanf("%d", &guess);
-		if (value < guess) {
+		count++;
+
+		if (guess > value)
 			printf("The suggested integer is high.\n");
-			count++;
-		}
-		else if (value > guess) {
+		if (guess < value)
 			printf("The suggested integer is low\n");
-			count++;
-		}
-		else if (value = guess) {
-			count++;
-			printf("Congratulations! Counts:%d\n", count);
-			break;
-		}
-	}
+	} while (value != guess);
+
+	printf("Congratulations! Count=%d\n", count);
 
 	return 0;
 }
